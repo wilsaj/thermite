a5926b9ac6b17d9ecb38a8c45972a7c21ddbef47
wilsaj <wilson.andrew.j+github@gmail.com>
2014-11-30 17:44:15 -0600

    basic sketch of functionality - change slides on click events

--- lib/thermite.jsx
+++ lib/thermite.jsx
@@ -1,17 +1,85 @@
 'use strict';
 
+var React = require('react/addons');
+
 var Slideshow = React.createClass({
+  getInitialState: function() {
+    return {
+      slides: [],
+      active: null
+    };
+  },
+  componentDidMount: function() {
+    var url = $('#slideshow').attr('data-url');
+
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({
+          slides: data.slides,
+          active: 0
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleClick: function(event) {
+    var next = this.state.active === this.state.slides.length - 1 ? 0 : this.state.active + 1;
+    this.setState({active: next});
+  },
   render: function() {
     return (
-      <div className="slides">
-        slides
+      <div onClick={this.handleClick}>
+        <SlideList slides={this.state.slides} active={this.state.active} />
       </div>
     );
   }
 });
 
+
+var SlideList = React.createClass({
+  render: function() {
+    var slides = this.props.slides;
+    var active = this.props.active;
+    var slideNodes = slides.map(function (slide, i) {
+      var key = 'react-' + i;
+      var slideActive = active == i;
+      return (
+        <Slide key={key} active={slideActive}>
+          {slide.text}
+        </Slide>
+      );
+    });
+    return (
+      <div className="slides">
+        {slideNodes}
+      </div>
+    );
+  }
+});
+
+
+var Slide = React.createClass({
+  render: function() {
+    var cx = React.addons.classSet;
+    var classes = cx({
+      'slide': true,
+      'hidden': !this.props.active
+    });
+
+    return (
+      <div className={classes}>
+        <span dangerouslySetInnerHTML={{__html: this.props.children.toString()}} />
+      </div>
+    );
+  }
+});
+
+
 React.render(
   <Slideshow />,
   document.getElementById('slideshow')
 );
-
