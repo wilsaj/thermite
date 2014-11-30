6fafed2d289e922bfc062ffabed851223388b3c3
wilsaj <wilson.andrew.j+github@gmail.com>
2014-11-30 11:34:24 -0600

    initial project setup

---
+++ lib/thermite.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+var Slideshow = React.createClass({
+  render: function() {
+    return (
+      <div className="slides">
+        slides
+      </div>
+    );
+  }
+});
+
+React.render(
+  <Slideshow />,
+  document.getElementById('slideshow')
+);
+
