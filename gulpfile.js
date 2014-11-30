57be0c7c7d4f9debc33e7f38166d0b7123e516d4
wilsaj <wilson.andrew.j+github@gmail.com>
2014-11-30 16:47:25 -0600

    add browserify support to gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,39 @@
 'use strict';
+var browserify = require('browserify');
 var gulp = require('gulp');
 var react = require('gulp-react');
+var transform = require('vinyl-transform');
 
+var dirs = {
+  dist: 'dist/',
+  tmp: '.tmp/'
+};
 var paths = {
   jsx: 'lib/**/*.jsx'
 };
 
 gulp.task('default', ['dist']);
 
-gulp.task('dist', function () {
+gulp.task('browserify', ['react'], function () {
+  var browserified = transform(function(filename) {
+    var b = browserify(filename);
+    return b.bundle();
+  });
+
+  return gulp.src(dirs.tmp + '/**/*.js')
+    .pipe(browserified)
+    .pipe(gulp.dest(dirs.dist));
+});
+
+
+gulp.task('dev', ['default', 'watch']);
+
+gulp.task('dist', ['react', 'browserify']);
+
+gulp.task('react', function () {
   return gulp.src(paths.jsx)
     .pipe(react())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(dirs.tmp));
 });
 
 gulp.task('watch', function () {
